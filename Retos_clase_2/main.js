3841836c0d056636e57d4ee99aaaf91cde0995b1
Martin Alarcon <xmartinalarx@gmail.com>
2022-10-24 20:06:49 -0300

    subida de retos 1 y 2

--- Retos_clase_2/main.js
+++ Retos_clase_2/main.js
@@ -1,3 +1,13 @@
+
+/* reto_1 */
+let n1 = Number(prompt ("Primera Nota"));
+let n2 = Number(prompt ("segunda nota"));
+let n3 = Number(prompt ("tercera nota"));
+
+console.log((n1 + n2 + n3) / 3)
+
+/* reto_2 */
+
 var edad = prompt("Introduce tu Edad")
 var estatura = prompt("Introduce tu estatura en Centrimetros")
 
